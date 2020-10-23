49b405c2b4c137cc756081d19b12cf0cd90ca4e4
mo <muhammadelkhateb95@gmail.com>
2020-10-23 16:37:49 +0200

    first push

---
+++ src/components/Todos.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <h3>{{ todo.title }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todos",
+  props: ["todos"],
+};
+</script>
+
+<style scoped></style>
