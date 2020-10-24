739899c49cb2c1e2680e5d7d799c34c721313717
mo <muhammadelkhateb95@gmail.com>
2020-10-24 12:50:17 +0200

    Add Routing Module

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea laboriosam
+      vitae sequi minus consectetur magni dolorum dolorem unde vel illo,
+      similique in, tempore, perspiciatis mollitia et laborum sed. Voluptatem,
+      dolore!
+    </p>
+  </div>
+</template>
